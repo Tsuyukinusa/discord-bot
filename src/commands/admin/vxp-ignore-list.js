3d2c31e751ee899cc6ca5ecf060ef8c4a3a4c445
Tsuyukinusa <sigmagma617@gmail.com>
2025-11-16 11:56:01 +0900

    Create vxp-ignore-list.js

---
+++ src/commands/admin/vxp-ignore-list.js
@@ -0,0 +1,30 @@
+import { SlashCommandBuilder, PermissionFlagsBits } from "discord.js";
+import { readGuildDB } from "../../utils/file.js";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("vxp-ignore-list")
+    .setDescription("VXPが増えないチャンネル一覧を表示します")
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
+
+  async execute(interaction) {
+    const guildId = interaction.guild.id;
+
+    const guildDB = await readGuildDB();
+    const list = guildDB[guildId]?.vxpIgnoreChannels || [];
+
+    if (list.length === 0) {
+      return interaction.reply({
+        content: "📭 除外チャンネルはありません！",
+        ephemeral: true,
+      });
+    }
+
+    const formatted = list.map(id => `• <#${id}>`).join("\n");
+
+    return interaction.reply({
+      content: `📌 **VXP除外チャンネル一覧**\n${formatted}`,
+      ephemeral: true,
+    });
+  },
+};
