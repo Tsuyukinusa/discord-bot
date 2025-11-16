fe8590c5819e99f7414476ef07372f95d39c550d
Tsuyukinusa <sigmagma617@gmail.com>
2025-11-16 19:55:08 +0900

    Create profile.js

---
+++ src/commands/leveling/profile.js
@@ -0,0 +1,30 @@
+import {
+  SlashCommandBuilder,
+  AttachmentBuilder,
+} from "discord.js";
+import { createProfileCard } from "../../services/profileService.js";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("profile")
+    .setDescription("プロフィールカードを表示します")
+    .addUserOption(opt =>
+      opt.setName("user")
+        .setDescription("表示するユーザー")
+        .setRequired(false)
+    ),
+
+  async execute(interaction, client) {
+    const user = interaction.options.getUser("user") || interaction.user;
+
+    await interaction.deferReply();
+
+    const buffer = await createProfileCard(interaction.guild.id, user);
+
+    const attachment = new AttachmentBuilder(buffer, {
+      name: "profile.png",
+    });
+
+    await interaction.editReply({ files: [attachment] });
+  },
+};
