aaeaba6d7530e9b88ae762d3e710d82b93e588c7
Tsuyukinusa <sigmagma617@gmail.com>
2025-12-22 15:13:52 +0900

    Update blackjackButtons.js

--- src/utils/gamble/blackjackButtons.js
+++ src/utils/gamble/blackjackButtons.js
@@ -1,40 +1,31 @@
 import { ActionRowBuilder, ButtonBuilder, ButtonStyle } from "discord.js";
-import { calcHand } from "./blackjackLogic.js";
 
 export function blackjackButtons(game) {
-  const hand = game.hands[game.currentHand];
-
-  const canDouble =
-    hand.length === 2 && !game.doubled && !game.split;
-
-  const canSplit =
-    hand.length === 2 &&
-    hand[0].value === hand[1].value &&
-    !game.split;
+  const disabled = game.finished;
 
   return new ActionRowBuilder().addComponents(
     new ButtonBuilder()
       .setCustomId("bj-hit")
       .setLabel("Hit")
       .setStyle(ButtonStyle.Primary)
-      .setDisabled(game.finished),
+      .setDisabled(disabled),
 
     new ButtonBuilder()
       .setCustomId("bj-stand")
       .setLabel("Stand")
       .setStyle(ButtonStyle.Success)
-      .setDisabled(game.finished),
+      .setDisabled(disabled),
 
     new ButtonBuilder()
       .setCustomId("bj-double")
       .setLabel("Double")
       .setStyle(ButtonStyle.Secondary)
-      .setDisabled(!canDouble),
+      .setDisabled(disabled || game.doubled || game.split),
 
     new ButtonBuilder()
       .setCustomId("bj-split")
       .setLabel("Split")
       .setStyle(ButtonStyle.Secondary)
-      .setDisabled(!canSplit)
+      .setDisabled(disabled || game.split)
   );
 }
