977dd3501f7ba7eb0097475dcb6bbb99310d214f
Tsuyukinusa <sigmagma617@gmail.com>
2025-12-22 15:28:03 +0900

    Update blackjack.js

--- src/massageCommands/gambling/blackjack.js
+++ src/massageCommands/gambling/blackjack.js
@@ -1,20 +1,6 @@
-// messageCommands/blackjack.js
-import { startBlackjack } from "../utils/gamble/blackjackCore.js";
-import { createBlackjackEmbed } from "../utils/gamble/blackjackEmbed.js";
-import { blackjackButtons } from "../utils/gamble/blackjackButtons.js";
-
-export default async function blackjackMessage(message, args, client) {
-  const bet = Number(args[0]);
-  if (!bet || bet <= 0) {
-    return message.reply("❌ 賭け金を指定してね");
+export default {
+  name: "blackjack",
+  async execute(message, args) {
+    message.reply("スラッシュコマンド `/blackjack` を使ってください！");
   }
-
-  const state = startBlackjack(bet);
-  client.blackjack = client.blackjack || {};
-  client.blackjack[message.author.id] = state;
-
-  await message.reply({
-    embeds: [createBlackjackEmbed(state)],
-    components: [blackjackButtons()],
-  });
-}
+};
