e92a19765c0b014e7aaf59224cd8ed2fb14bf2e5
Tsuyukinusa <sigmagma617@gmail.com>
2025-12-31 19:50:55 +0900

    Update omikuji-config-set-weight.js

--- src/commands/admin/omikuji-config-set-weight.js
+++ src/commands/admin/omikuji-config-set-weight.js
@@ -1,6 +1,6 @@
 // commands/admin/omikuji-config-set-weight.js
 import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
-import { readGuildDB, writeGuildDB } from "../../utils/file.js";
+import { readGuildDB, writeGuildDB } from "../../utils/core/file.js";
 
 export default {
     data: new SlashCommandBuilder()
