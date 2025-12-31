3e138da898b77eb716df4c94d00c60bf5c5586a9
Tsuyukinusa <sigmagma617@gmail.com>
2025-12-31 15:44:22 +0900

    Create pokerSelect.js

---
+++ src/selects/pokerSelect.js
@@ -0,0 +1,20 @@
+// src/selects/pokerSelect.js
+import { getPokerGame, savePokerGame } from "../utils/gamble/pokerStore.js";
+
+export default {
+  customId: "poker-select",
+
+  async execute(interaction) {
+    const guildId = interaction.guild.id;
+    const userId = interaction.user.id;
+
+    const game = getPokerGame(guildId, userId);
+    if (!game) return interaction.deferUpdate();
+
+    // 選択されたカード index 配列
+    game.exchangeIndexes = interaction.values.map(v => Number(v));
+    savePokerGame(guildId, userId, game);
+
+    await interaction.deferUpdate();
+  }
+};
