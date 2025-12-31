7e57a59462d08ded35aad1badfcde474cbeef0af
Tsuyukinusa <sigmagma617@gmail.com>
2025-12-31 19:59:08 +0900

    Update balance.js

--- src/commands/economy/balance.js
+++ src/commands/economy/balance.js
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
-import { getUser, getAllUsers } from "../../utils/userdb.js";
+import { getUser, getAllUsers } from "../../utils/core/file.js";
 
 export default {
     data: new SlashCommandBuilder()
