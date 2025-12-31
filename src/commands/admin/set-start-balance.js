08ca3873f404447d69370e64b2f5a6fa1f9cc775
Tsuyukinusa <sigmagma617@gmail.com>
2025-12-31 19:55:12 +0900

    Update set-start-balance.js

--- src/commands/admin/set-start-balance.js
+++ src/commands/admin/set-start-balance.js
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder, PermissionFlagsBits } from "discord.js";
-import { readGuildDB, writeGuildDB } from "../../utils/file.js";
+import { readGuildDB, writeGuildDB } from "../../utils/core/file.js";
 
 export default {
   data: new SlashCommandBuilder()
