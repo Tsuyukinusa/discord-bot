0e02ca77728624f577e35a48420ec052b345c0a5
Tsuyukinusa <sigmagma617@gmail.com>
2025-12-31 10:00:58 +0900

    Update jackpot.js

--- src/commands/gambling/jackpot.js
+++ src/commands/gambling/jackpot.js
@@ -1,1 +1,39 @@
+import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
+import { startJackpot, getJackpot } from "../../utils/gamble/jackpotCore.js";
+import { createJackpotEmbed } from "../../utils/gamble/jackpotEmbed.js";
+import { jackpotButtons } from "../../utils/gamble/jackpotButtons.js";
 
+export default {
+  data: new SlashCommandBuilder()
+    .setName("jackpot")
+    .setDescription("ジャックポットを開始")
+    .addIntegerOption(o =>
+      o.setName("entry")
+        .setDescription("参加費")
+        .setRequired(true)
+        .setMinValue(1)
+    ),
+
+  async execute(interaction) {
+    const guildId = interaction.guild.id;
+    const entry = interaction.options.getInteger("entry");
+
+    const jackpot = startJackpot({
+      guildId,
+      hostId: interaction.user.id,
+      entry
+    });
+
+    if (jackpot.error) {
+      return interaction.reply({
+        embeds: [new EmbedBuilder().setColor("Red").setDescription(jackpot.error)],
+        ephemeral: true
+      });
+    }
+
+    return interaction.reply({
+      embeds: [createJackpotEmbed(jackpot)],
+      components: [jackpotButtons()]
+    });
+  }
+};
