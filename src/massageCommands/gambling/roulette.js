bf993b16e306cd6bf272a979bed9eb4becfa953b
Tsuyukinusa <sigmagma617@gmail.com>
2025-12-31 14:09:17 +0900

    Create roulette.js

---
+++ src/massageCommands/gambling/roulette.js
@@ -0,0 +1,38 @@
+import {
+  joinRoulette,
+  getRoulette
+} from "../utils/gamble/rouletteCore.js";
+import { createRouletteWaitingEmbed } from "../utils/gamble/rouletteEmbed.js";
+
+export default {
+  name: "roulette",
+
+  async execute(message, args) {
+    const [amountArg, type, value] = args;
+
+    const amount =
+      amountArg === "half" || amountArg === "all"
+        ? amountArg
+        : Number(amountArg);
+
+    const bet = {
+      amount,
+      type,
+      value: value ? Number(value) : null
+    };
+
+    const result = await joinRoulette({
+      guildId: message.guild.id,
+      userId: message.author.id,
+      bet
+    });
+
+    if (result?.error) {
+      return message.reply(result.error);
+    }
+
+    return message.reply({
+      embeds: [createRouletteWaitingEmbed(getRoulette(message.guild.id))]
+    });
+  }
+};
