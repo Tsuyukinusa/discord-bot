013b4ec6a6b1be4582a2c2fa599d9f7c84c245ef
Tsuyukinusa <sigmagma617@gmail.com>
2026-01-01 16:49:56 +0900

    Update cooldownService.js

--- src/services/cooldownService.js
+++ src/services/cooldownService.js
@@ -1,1 +1,22 @@
+export function initCooldown(user) {
+  if (!user.cooldowns) user.cooldowns = {};
+}
 
+export function isOnCooldown(user, key, cooldownMs) {
+  initCooldown(user);
+
+  const last = user.cooldowns[key];
+  if (!last) return false;
+
+  return Date.now() - last < cooldownMs;
+}
+
+export function getRemaining(user, key, cooldownMs) {
+  const passed = Date.now() - user.cooldowns[key];
+  return Math.ceil((cooldownMs - passed) / 1000);
+}
+
+export function setCooldown(user, key) {
+  initCooldown(user);
+  user.cooldowns[key] = Date.now();
+}
