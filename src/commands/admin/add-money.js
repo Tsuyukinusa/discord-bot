5fd426fe8b3783b69e160e8a59b2787d9d9ef855
Tsuyukinusa <sigmagma617@gmail.com>
2026-01-01 10:38:09 +0900

    Update add-money.js

--- src/commands/admin/add-money.js
+++ src/commands/admin/add-money.js
@@ -1,1 +1,39 @@
+// src/commands/admin/addmoney.js
+import { SlashCommandBuilder, PermissionFlagsBits } from "discord.js";
+import { addBalance } from "../../Services/economyServices.js";
 
+export const data = new SlashCommandBuilder()
+  .setName("addmoney")
+  .setDescription("指定したユーザーにお金を追加します（管理者用）")
+  .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+  .addUserOption(opt =>
+    opt
+      .setName("user")
+      .setDescription("対象ユーザー")
+      .setRequired(true)
+  )
+  .addIntegerOption(opt =>
+    opt
+      .setName("amount")
+      .setDescription("追加する金額")
+      .setRequired(true)
+  );
+
+export async function execute(interaction) {
+  const user = interaction.options.getUser("user");
+  const amount = interaction.options.getInteger("amount");
+  const guildId = interaction.guildId;
+
+  if (amount <= 0) {
+    return interaction.reply({
+      content: "金額は正の数で指定してください。",
+      ephemeral: true
+    });
+  }
+
+  await addBalance(guildId, user.id, amount);
+
+  return interaction.reply({
+    content: `💰 <@${user.id}> に **${amount}** 追加しました。`
+  });
+}
