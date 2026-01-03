3d8f704c9fb1185c797d9235b40a31f855b99c2b
Tsuyukinusa <sigmagma617@gmail.com>
2026-01-03 19:41:02 +0900

    Update set-role-income.js

--- src/commands/admin/set-role-income.js
+++ src/commands/admin/set-role-income.js
@@ -1,1 +1,47 @@
+// src/commands/economy/set-role-income.js
+import {
+  SlashCommandBuilder,
+  PermissionFlagsBits
+} from "discord.js";
+import { readGuildDB, writeGuildDB } from "../../utils/core/file.js";
 
+export default {
+  data: new SlashCommandBuilder()
+    .setName("set-role-income")
+    .setDescription("リアクション給料が発生するロールを設定します")
+    .addRoleOption(opt =>
+      opt.setName("role")
+        .setDescription("給料対象のロール")
+        .setRequired(true)
+    )
+    .addIntegerOption(opt =>
+      opt.setName("amount")
+        .setDescription("リアクション1個あたりの給料")
+        .setMinValue(1)
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
+
+  async execute(interaction) {
+    const guildId = interaction.guild.id;
+    const role = interaction.options.getRole("role");
+    const amount = interaction.options.getInteger("amount");
+
+    const db = await readGuildDB();
+
+    if (!db[guildId]) db[guildId] = {};
+    if (!db[guildId].income) db[guildId].income = {};
+    if (!db[guildId].income.roles) db[guildId].income.roles = {};
+
+    db[guildId].income.roles[role.id] = {
+      payPerReaction: amount
+    };
+
+    await writeGuildDB(db);
+
+    return interaction.reply({
+      content: `✅ ロール **${role.name}** を給料対象に設定しました\n💰 1リアクション = **${amount}**`,
+      ephemeral: true
+    });
+  }
+};
